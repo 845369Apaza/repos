5d51367daa9b56676de32ad75094729d215f48b8
apazagab@outlook.com.ar <apazagab@outlook.com.ar>
2021-03-22 19:33:47 -0300

    Agregar archivos de proyecto.

---
+++ tp1/tp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A845369Actividad01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("oprima una tecla para salir");
+            Console.ReadKey();
+                                }
+    }
+}
